a861886f123226ce83824df91f8440ebb6dd5a36
Pavel Savelev <9173912682pavel@gmail.com>
2026-04-01 14:49:07 +0300

    fix audit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,43 +1,51 @@
-import { errors } from 'celebrate'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
 import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import rateLimit from 'express-rate-limit'
 import mongoose from 'mongoose'
 import path from 'path'
+import { errors } from 'celebrate'
 import { DB_ADDRESS } from './config'
-import errorHandler from './middlewares/error-handler'
-import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
+import serveStatic from './middlewares/serverStatic'
+import errorHandler from './middlewares/error-handler'
 
 const { PORT = 3000 } = process.env
 const app = express()
 
-app.use(cookieParser())
-
+// Body parsers
+app.use(cookieParser('super-secret-key'))
 app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(json())
+app.use(urlencoded({ extended: true }))
+
+const limiter = rateLimit({
+  windowMs: 10 * 1000,
+  // max: 10,
+  keyGenerator: (req) => req.ip || '',
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'Слишком много запросов, повторите позже',
+})
+
+app.use(limiter)
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
-app.use(urlencoded({ extended: true }))
-app.use(json())
-
-app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
 
-// eslint-disable-next-line no-console
-
 const bootstrap = async () => {
-    try {
-        await mongoose.connect(DB_ADDRESS)
-        await app.listen(PORT, () => console.log('ok'))
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    await mongoose.connect(DB_ADDRESS)
+    await app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    )
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 bootstrap()
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import csrf from 'csurf'
 import {
     getCurrentUser,
     getCurrentUserRoles,
@@ -11,13 +12,24 @@ import {
 import auth from '../middlewares/auth'
 
 const authRouter = Router()
+const csrfProtection = csrf({
+  cookie: {
+    signed: true
+  }
+})
+
+authRouter.get('/csrf-token', csrfProtection, (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 authRouter.get('/user', auth, getCurrentUser)
 authRouter.patch('/me', auth, updateCurrentUser)
 authRouter.get('/user/roles', auth, getCurrentUserRoles)
 authRouter.post('/login', login)
+// authRouter.post('/login', login)
 authRouter.get('/token', refreshAccessToken)
 authRouter.get('/logout', logout)
-authRouter.post('/register', register)
+authRouter.post('/register', csrfProtection, register)
+// authRouter.post('/register', register)
 
 export default authRouter
