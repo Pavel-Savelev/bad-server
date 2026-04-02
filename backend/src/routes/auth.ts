c41508c957452f1d113bcb71f743ecb822cb6414
Pavel Savelev <9173912682pavel@gmail.com>
2026-04-02 14:58:37 +0300

    fix csrf

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,3 +1,4 @@
+import csurf from 'csurf'
 import { Router } from 'express'
 import {
     getCurrentUser,
@@ -9,20 +10,20 @@ import {
     updateCurrentUser,
 } from '../controllers/auth'
 import auth from '../middlewares/auth'
-import { generateCsrfToken, verifyCsrfToken } from '../middlewares/csrf'
 
 const authRouter = Router()
 
-authRouter.get('/csrf-token', generateCsrfToken)
+const csrfProtection = csurf({ cookie: true })
 
+authRouter.get('/auth/csrf-token', csrfProtection, (req, res) => {
+    res.json({ csrfToken: req.csrfToken() })
+})
 authRouter.get('/user', auth, getCurrentUser)
-authRouter.patch('/me', auth, verifyCsrfToken, updateCurrentUser)
+authRouter.patch('/me', auth, csrfProtection, updateCurrentUser)
 authRouter.get('/user/roles', auth, getCurrentUserRoles)
 authRouter.post('/login', login)
-// authRouter.post('/login', login)
 authRouter.get('/token', refreshAccessToken)
 authRouter.get('/logout', logout)
-authRouter.post('/register', verifyCsrfToken, register)
-// authRouter.post('/register', register)
+authRouter.post('/register',csrfProtection, register)
 
 export default authRouter
