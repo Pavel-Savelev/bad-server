f11e28551dfde5de2227f3eef698d95071d37137
Pavel Savelev <9173912682pavel@gmail.com>
2026-04-02 09:51:10 +0300

    fix file rules

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
+import imageSize from 'image-size'
+import fs from 'fs'
 import BadRequestError from '../errors/bad-request-error'
+
 
 const MAX_FILE_SIZE = 10 * 1024 * 1024
 const MIN_FILE_SIZE = 2 * 1024
@@ -11,18 +14,29 @@ export const uploadFile = async (
     next: NextFunction
 ) => {
 
-    
+
 
     if (!req.file) {
         return next(new BadRequestError('Файл не загружен'))
     }
-    
+
     if (req.file.size < MIN_FILE_SIZE) {
-       return next(new BadRequestError('Файл слишком маленький'));
+        return next(new BadRequestError('Файл слишком маленький'));
     }
 
     if (req.file.size > MAX_FILE_SIZE) {
-       return next(new BadRequestError('Файл слишком большой'));
+        return next(new BadRequestError('Файл слишком большой'));
+    }
+
+    try {
+        // читаем файл как Buffer
+        const buffer = fs.readFileSync(req.file.path)
+        const dimensions = imageSize(buffer) // теперь TypeScript согласен
+        if (!dimensions.width || !dimensions.height) {
+            return next(new BadRequestError('Некорректное изображение'))
+        }
+    } catch (err) {
+        return next(new BadRequestError('Некорректное изображение'))
     }
 
     try {
