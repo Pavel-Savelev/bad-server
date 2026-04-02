5190b79d9482788294a8f4b0decc9e2cd720ceeb
Pavel Savelev <9173912682pavel@gmail.com>
2026-04-02 13:02:47 +0300

    fix дшьшеы

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,7 +7,6 @@ import customerRouter from './customers'
 import orderRouter from './order'
 import productRouter from './product'
 import uploadRouter from './upload'
-import { customersRateLimiter, validateCustomerQuery } from '../middlewares/rate-limit'
 
 const router = Router()
 
@@ -15,7 +14,7 @@ router.use('/auth', authRouter)
 router.use('/product', productRouter)
 router.use('/order', auth, orderRouter)
 router.use('/upload', auth, uploadRouter)
-router.use('/customers', auth, customersRateLimiter, validateCustomerQuery, customerRouter)
+router.use('/customers', auth, customerRouter)
 
 router.use((_req: Request, _res: Response, next: NextFunction) => {
     next(new NotFoundError('Маршрут не найден'))
