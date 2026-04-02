0dd2e42c8d9ce847047e106e6d716622a227289c
Pavel Savelev <9173912682pavel@gmail.com>
2026-04-02 10:26:44 +0300

    fix file rules

--- backend/src/middlewares/rate-limit.ts
+++ backend/src/middlewares/rate-limit.ts
@@ -1,8 +1,16 @@
 import rateLimit from 'express-rate-limit';
-
+import { celebrate, Joi, Segments } from 'celebrate';
 
 export const customersRateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, 
   max: 10, 
   message: 'Слишком много запросов с этого IP, попробуйте позже',
+});
+
+
+export const validateCustomerQuery = celebrate({
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().min(1).default(1),
+    limit: Joi.number().min(1).max(10).default(10), // лимит максимум 10
+  }),
 });
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,14 +1,13 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import NotFoundError from '../errors/not-found-error'
 
-
 import auth from '../middlewares/auth'
 import authRouter from './auth'
 import customerRouter from './customers'
 import orderRouter from './order'
 import productRouter from './product'
 import uploadRouter from './upload'
-import { customersRateLimiter } from '../middlewares/rate-limit'
+import { customersRateLimiter, validateCustomerQuery } from '../middlewares/rate-limit'
 
 const router = Router()
 
@@ -16,7 +15,7 @@ router.use('/auth', authRouter)
 router.use('/product', productRouter)
 router.use('/order', auth, orderRouter)
 router.use('/upload', auth, uploadRouter)
-router.use('/customers', auth, customersRateLimiter, customerRouter)
+router.use('/customers', auth, customersRateLimiter, validateCustomerQuery, customerRouter)
 
 router.use((_req: Request, _res: Response, next: NextFunction) => {
     next(new NotFoundError('Маршрут не найден'))
